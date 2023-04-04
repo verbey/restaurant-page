d8ff450886d7252a578d9b9ed4d443fa7be48f43
Verbiturum <user@example.com>
2023-04-05 01:02:10 +0300

    Add menu.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { appendHomeContent } from "./home.js";
 import { appendContactContent } from "./contact.js";
+import { appendMenuContent } from "./menu.js";
 
 if (Math.floor(Math.random() * 2) === 0) appendHomeContent();
-else appendContactContent();
+else if (Math.floor(Math.random() * 2) === 0) appendContactContent();
+else appendMenuContent();
---
+++ src/menu.js
@@ -0,0 +1,14 @@
+function appendMenuContent() {
+
+	const content = document.querySelector("#content");
+
+	const h2 = document.createElement("h2");
+	h2.textContent = "Borscht";
+	content.appendChild(h2);
+
+	const borscht = document.createElement("div");
+	borscht.textContent = "Something something, Ukrainian borscht, hurr durr";
+	content.appendChild(borscht);
+
+}
+export { appendMenuContent };
